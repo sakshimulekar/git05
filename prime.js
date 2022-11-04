a63a8635c6230d5465802cd335cedc3d788d6523
sakshimulekar <sakshimulekar2017@gmail.com>
2022-11-04 16:54:22 +0530

    by sakshi

--- prime.js
+++ prime.js
@@ -1,1 +1,17 @@
-count = 0;
+let num =13;
+let count = 0;
+for(let i =2; i<=13; i++)
+{
+	if(num%i==0)
+{
+	count++;
+}
+}
+if( count == 1)
+{
+console.log("prime");
+}
+else {
+console.log("not prime")
+}
+
